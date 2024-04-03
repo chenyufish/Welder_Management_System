5afe9d963036a4f596d0a0fc394afd18c2ad34d0
fishman <cy13692810010@163.com>
2024-04-04 00:53:58 +0800

    改一改前端页面

--- WelderManager-front/src/pages/SearchPage.vue
+++ WelderManager-front/src/pages/SearchPage.vue
@@ -2,106 +2,196 @@
   <form action="/">
     <van-search
         v-model="searchText"
-        show-action
-        placeholder="请输入搜索标签"
+        placeholder="请输入要搜索的标签"
         @search="onSearch"
-        @cancel="onCancel"
     />
+    <div v-if="activeIds.length > 0">
+      <van-divider content-position="left">已选择标签</van-divider>
+      <van-row style="padding: 16px">
+        <van-col v-for="tag in activeIds">
+          <van-tag closeable size="small" type="primary" @close="close(tag)" style="margin: 5px">
+            {{ tag }}
+          </van-tag>
+        </van-col>
+      </van-row>
+    </div>
+    <van-divider content-position="left">选择标签</van-divider>
+    <van-tree-select
+        v-model:active-id="activeIds"
+        v-model:main-active-index="activeIndex"
+        :items="tagList"
+    />
+    <van-divider content-position="left">自定义标签</van-divider>
+    <van-cell-group inset>
+      <van-field v-model="userDefinedTag" placeholder="请输入标签" style="padding-right: 5px">
+        <template #button>
+          <van-button size="small" type="primary" @click="addUserDefinedTag">添加</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
   </form>
-  <van-divider content-position="left">已选标签</van-divider>
-  <div v-if="activeIds.length === 0">请选择标签</div>
-  <van-row gutter="16" style="padding: 0 16px">
-    <van-col v-for="tag in activeIds">
-      <van-tag  closeable size="small" type="primary" @close="doClose(tag)">
-        {{ tag }}
-      </van-tag>
-    </van-col>
-  </van-row>
-
-  <van-divider content-position="left">已选标签</van-divider>
-  <van-tree-select
-      v-model:active-id="activeIds"
-      v-model:main-active-index="activeIndex"
-      :items="tagList"
-  />
-  <div style="padding :16px">
-    <van-button block type="primary" @click="doSearchResult">搜索</van-button>
+  <div style="margin: 20px">
+    <van-button block type="primary" @click="searchUser">搜索</van-button>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+
+import {ref} from "vue";
 import {useRouter} from "vue-router";
+import {showFailToast} from "vant";
 
-const searchText = ref('');
-
-
-
-const router =useRouter();
-const doSearchResult =()=>{
-  router.push({
-    path: '/user/list',
-    query:{
-      tags:activeIds.value
-    }
-  })
+const userDefinedTag = ref("")
+const addUserDefinedTag = () => {
+  if (userDefinedTag.value !== "") {
+    activeIds.value.push(userDefinedTag.value)
+    userDefinedTag.value = ""
+  } else {
+    showFailToast("请先输入标签名")
+  }
 }
-const originTagList = [{
-  text: '性别',
-  children: [
-    { text: '男', id: '男' },
-    { text: '女', id: '女' },
-    { text: '嬲', id: '嬲' },
-  ],
-}, {
-  text: '年级',
-  children: [
-    { text: '大一', id: '大一' },
-    { text: '大二', id: '大二' },
-    { text: '大三', id: '大三' },
-    { text: '大四', id: '大四' },
-    { text: '大五', id: '大五' },
-    { text: '大六', id: '大六' },
-  ],
-},
+let router = useRouter();
+const originTagList = [
+  {
+    text: '性别',
+    children: [
+      {text: '男', id: '男'},
+      {text: '女', id: '女'},
+      {text: '保密', id: '保密'}
+    ],
+  },
+  {
+    text: '年级',
+    children: [
+      {text: '高一', id: '高一'},
+      {text: '高二', id: '高二'},
+      {text: '高三', id: '高三'},
+      {text: '大一', id: '大一'},
+      {text: '大二', id: '大二'},
+      {text: '大三', id: '大三'},
+      {text: '大四', id: '大四'},
+      {text: '研究生', id: '研究生'},
+      {text: '已工作', id: '已工作'},
+    ],
+  },
+  {
+    text: "编程",
+    children: [
+      {text: "Java", id: "Java"},
+      {text: "Python", id: "Python"},
+      {text: "C++", id: "C++"},
+      {text: "JavaScript", id: "JavaScript"},
+      {text: "Ruby", id: "Ruby"},
+      {text: "PHP", id: "PHP"},
+      {text: "Swift", id: "Swift"},
+      {text: "Objective-C", id: "Objective-C"},
+      {text: "C#", id: "C#"},
+      {text: "Go", id: "Go"},
+      {text: "Kotlin", id: "Kotlin"},
+      {text: "TypeScript", id: "TypeScript"},
+      {text: "Rust", id: "Rust"},
+      {text: "Lua", id: "Lua"},
+      {text: "Perl", id: "Perl"},
+      {text: "Scala", id: "Scala"},
+      {text: "Dart", id: "Dart"},
+      {text: "R", id: "R"},
+      {text: "MATLAB", id: "MATLAB"},
+      {text: "Haskell", id: "Haskell"},
+      {text: "Julia", id: "Julia"},
+      {text: "Groovy", id: "Groovy"},
+      {text: "Shell", id: "Shell"},
+      {text: "Assembly", id: "Assembly"},
+      {text: "F#", id: "F#"},
+      {text: "Clojure", id: "Clojure"},
+      {text: "Erlang", id: "Erlang"},
+      {text: "Lisp", id: "Lisp"},
+      {text: "Prolog", id: "Prolog"},
+      {text: "Smalltalk", id: "Smalltalk"}
+    ]
+  },
+  {
+    text: '兴趣爱好',
+    children: [
+      {text: '阅读', id: '阅读'},
+      {text: '旅游', id: '旅游'},
+      {text: '游戏', id: '游戏'},
+      {text: '音乐', id: '音乐'},
+      {text: '电影', id: '电影'},
+      {text: '美食', id: '美食'},
+      {text: '健身', id: '健身'},
+      {text: '编程', id: '编程'},
+      {text: '摄影', id: '摄影'},
+      {text: '绘画', id: '绘画'},
+      {text: '写作', id: '写作'},
+      {text: '手工', id: '手工'},
+      {text: '园艺', id: '园艺'},
+      {text: '钓鱼', id: '钓鱼'},
+      {text: '篮球', id: '篮球'},
+      {text: '足球', id: '足球'},
+      {text: '棒球', id: '棒球'},
+      {text: '乒乓球', id: '乒乓球'},
+      {text: '羽毛球', id: '羽毛球'},
+      {text: '网球', id: '网球'}
+    ]
+  },
+  {
+    text: '职业',
+    children: [
+      {text: '医生', id: '医生'},
+      {text: '律师', id: '律师'},
+      {text: '教师', id: '教师'},
+      {text: '工程师', id: '工程师'},
+      {text: '程序员', id: '程序员'},
+      {text: '设计师', id: '设计师'},
+      {text: '销售', id: '销售'},
+      {text: '市场营销', id: '市场营销'},
+      {text: '会计师', id: '会计师'},
+      {text: '金融分析师', id: '金融分析师'},
+      {text: '投资银行家', id: '投资银行家'},
+      {text: '记者', id: '记者'},
+      {text: '编辑', id: '编辑'},
+      {text: '作家', id: '作家'},
+      {text: '演员', id: '演员'},
+      {text: '导演', id: '导演'},
+      {text: '音乐家', id: '音乐家'},
+      {text: '画家', id: '画家'},
+      {text: '建筑师', id: '建筑师'},
+      {text: '厨师', id: '厨师'},
+      {text: '学生', id: '学生'}
+    ]
+  }
 ];
-//标签列表
 let tagList = ref(originTagList);
-/**
- *  搜索过滤
- * @param val
- */
+const searchText = ref('');
 const onSearch = () => {
-  tagList.value = originTagList.map(parentTag =>{
-    const tempChildren =  [...parentTag.children];
-    const tempParentTag =  {...parentTag};
+  tagList.value = originTagList.map(parentTag => {
+    const tempChildren = [...parentTag.children];
+    const tempParentTag = {...parentTag};
     tempParentTag.children = tempChildren.filter(item => item.text.includes(searchText.value))
     return tempParentTag;
   })
 };
-
-//取消  清空
-const onCancel = () => {
-  searchText.value = '';
-  tagList.value = originTagList;
-};
-
-
-//已选中的标签
 const activeIds = ref([]);
 const activeIndex = ref(0);
 
-
-//关闭标签
-const  doClose = (tag) =>{
-  activeIds.value = activeIds.value.filter(item =>{
+const close = (tag) => {
+  activeIds.value = activeIds.value.filter((item) => {
     return item !== tag;
   })
-
+};
+const searchUser = () => {
+  if (activeIds.value.length === 0) {
+    showFailToast("标签不能为空")
+  } else {
+    router.push({
+      path: '/search/userList',
+      query: {
+        tags: activeIds.value
+      }
+    })
+  }
 }
-
 </script>
 
 <style scoped>
-
 </style>
--- WelderManager-front/src/pages/SearchResultPage.vue
+++ WelderManager-front/src/pages/SearchResultPage.vue
@@ -1,73 +1,81 @@
 <template>
-  <van-card
-      v-for="user in userList"
-      :desc="user.profile"
-      :title="`${user.username} (${user.planetCode})`"
-      :thumb="user.avatarUrl"
-  >
-    <template #tags>
-      <van-tag plain type="danger" v-for="tag in tags" style="margin-right: 8px; margin-top: 8px" >
-        {{tag}}
-      </van-tag>
-    </template>
-    <template #footer>
-      <van-button size="mini">联系我</van-button>
-    </template>
-  </van-card>
-  <van-empty v-if="!userList || userList.length < 1" description="搜索结果为空" />
+  <div style="position: relative;height: 100%;width: 100%">
+    <van-pull-refresh
+        v-model="refreshLoading"
+        success-text="刷新成功"
+        @refresh="onRefresh"
+    >
+      <van-list
+          v-model:loading="listLoading"
+          :finished="listFinished"
+          offset="0"
+          finished-text="没有更多了"
+          @load="onLoad"
+      >
+        <UserCardList :user-list="userList"/>
+      </van-list>
+    </van-pull-refresh>
+    <van-back-top right="20px" bottom="60px"/>
+    <van-empty v-if="(!userList ||　userList.length===0) && listLoading===false" image="search"
+               description="暂无符合要求的用户"/>
+  </div>
 </template>
 
-<script setup >
-import {onMounted, ref} from "vue";
+<script setup>
 import {useRoute} from "vue-router";
-import {showFailToast, showSuccessToast} from "vant";
-
+import {ref} from "vue";
 import myAxios from "../plugins/myAxios.js";
+import {showFailToast} from "vant";
+import qs from 'qs';
+import UserCardList from "../components/UserCardList.vue";
 
-import qs from 'qs'
+let route = useRoute();
+const {tags} = route.query
+const userList = ref([])
+const currentPage = ref(0)
+const refreshLoading = ref(false)
+const listLoading = ref(false)
+const listFinished = ref(false)
 
-const route = useRoute();
-const {tags} = route.query;
-
-
-const userList = ref([]);
-
-
-onMounted( async () =>{
-  // 为给定 ID 的 user 创建请求
-  const userListData = await  myAxios.get('/user/search/tags',{
-    withCredentials: true,
+async function getSearchResult(currentPage) {
+  const res = await myAxios.get("/user/search/tags?currentPage=" + currentPage, {
     params: {
       tagNameList: tags
     },
-
-    //序列化
-    paramsSerializer: {
-      serialize: params => qs.stringify(params, { indices: false}),
+    paramsSerializer: params => {
+      return qs.stringify(params, {indices: false})
     }
   })
-      .then(function (response) {
-        console.log('/user/search/tags succeed',response);
-        showSuccessToast('请求成功');
-        return response?.data;
+  if (res.data.code === 0) {
+    if (res.data.data.records.length !== 0) {
+      res.data.data.records.forEach(user => {
+        if (user.tags) {
+          user.tags = JSON.parse(user.tags)
+        }
       })
-      //todo 减少了一层data的读取，即可以成功解析出用户数据
-      .catch(function (error) {
-        console.log('/user/search/tags error',error);
-        showFailToast('请求失败');
-      });
-  if (userListData){
-    userListData.forEach(user =>{
-      if (user.tags){
-        user.tags = JSON.parse(user.tags);
-      }
-    })
-    userList.value = userListData;
+      res.data.data.records.forEach((item) => userList.value.push(item))
+    } else {
+      listFinished.value = true
+    }
+  } else {
+    showFailToast("搜索失败" + (res.data.description ? `,${res.data.description}` : ''))
+    listFinished.value = true
   }
-})
+}
 
+const onLoad = async () => {
+  currentPage.value++
+  await getSearchResult(currentPage.value)
+  listLoading.value = false;
+}
 
-
+const onRefresh = async () => {
+  currentPage.value = 1
+  userList.value = []
+  await getSearchResult(currentPage.value)
+  refreshLoading.value = false
+  listLoading.value = false;
+}
 </script>
 
 <style scoped>
